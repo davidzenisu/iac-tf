b436273f75f5b752084d1a7760ee4ff53ee3935c
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2026-08-03 14:20:08 +0000

    Remove unnecessary provider details

--- providers.tf
+++ providers.tf
@@ -6,6 +6,5 @@ provider "cloudflare" {
 }
 
 provider "googleplay" {
-  service_account_json_base64 = filebase64(pathexpand("~/service-account.json"))
   developer_id                = var.play_store_developer_id
 }
